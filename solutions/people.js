7b211aba74f29e04dfbb4797003343d1dd9ea8fa
AjayMarukonda22 <ajaymarukonda12@gmail.com>
2025-02-15 12:32:12 +0530

    completed Average salary per profession

--- solutions/people.js
+++ solutions/people.js
@@ -14,14 +14,14 @@ import { people } from "../data/people.js";
 //     return result;
 // }
 
-const EngineersWithGivenAge = (people) => {
-    if(!people || people.length === 0 || !Array.isArray(people))
-        return "invalid input";
+// const EngineersWithGivenAge = (people) => {
+//     if(!people || people.length === 0 || !Array.isArray(people))
+//         return "invalid input";
 
-    return people
-                 .filter((person) => person.profession === "Engineer" && person.age > 30)
-}
-console.log(EngineersWithGivenAge(people));
+//     return people
+//                  .filter((person) => person.profession === "Engineer" && person.age > 30)
+// }
+// console.log(EngineersWithGivenAge(people));
 
 //Group Users based on profession
 // const groupUsersByProfession = (people) => {
@@ -50,4 +50,55 @@ const groupUsersByProfession = (people) => {
     return output;
 }
 
-console.log(groupUsersByProfession(people));
+// console.log(groupUsersByProfession(people));
+
+//Average salary per profession
+// const averageSalaryPerProfession = (people) => {
+
+//     let output = {};
+//     for(let person of people) {
+//         let profession = person.profession;
+//         let salary = person.salary;
+//         if(!output[profession]) {
+//             output[profession] = {totalSalary: 0, count : 0};
+//         }
+//         output[profession].totalSalary += salary;
+//         output[profession].count += 1;
+//     }
+    
+//     let result = {};
+//     for(let profession in output) {
+//         let totalSalary = output[profession].totalSalary;
+//         let count = output[profession].count;
+//          result[profession] = {
+//             averageSalary : Math.round((totalSalary/count) * 100) /100
+//          }
+//     }
+//     return result;
+// }
+
+const averageSalaryPerProfession = (people) => {
+   
+    let output = people.reduce((acc, person) => {
+        let profession = person.profession;
+        let salary = person.salary;
+      if(!acc[profession]) {
+            acc[profession] = {totalSalary: 0, count : 0 }
+      }
+        acc[profession].totalSalary += salary;
+         acc[profession].count += 1;
+         return acc;
+
+    }, {});
+
+    return Object.entries(output)
+                                .map(([ profession, salaryObject]) => {
+                                        return {
+                                            [profession]: {
+                                                averageSalary: Math.round((salaryObject.totalSalary/salaryObject.count) * 100) / 100
+                                            }
+                                        }
+                                              });
+                    
+}
+console.log(averageSalaryPerProfession(people));
